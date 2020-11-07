a858877a5dd4906f47f91d6a1d4b30f1bede8f82
upsidedown8 <tomthorogood@outlook.com>
2020-11-07 16:47:26 +0000

    added console/gui main methods

---
+++ src/gui_game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <QtWidgets>
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(
+        QApplication::translate("toplevel", "Top-level widget")
+    );
+
+    return app.exec();
+}
