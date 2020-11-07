507f0d4ffdce8865512480043fd08dfe3d473c9d
upsidedown8 <tomthorogood@outlook.com>
2020-11-07 17:00:38 +0000

    use sfml instead of qt

--- src/gui_game.cpp
+++ src/gui_game.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <QtWidgets>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char **argv) {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.resize(320, 240);
-    window.show();
-    window.setWindowTitle(
-        QApplication::translate("toplevel", "Top-level widget")
-    );
+    sf::RenderWindow window(sf::VideoMode(300, 300), "Tic Tac Toe");
+    sf::CircleShape shape(100.0f);
+    shape.setFillColor(sf::Color::Green);
 
-    return app.exec();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+    return 0;
 }
